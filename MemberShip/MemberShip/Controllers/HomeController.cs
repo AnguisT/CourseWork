491e17e3ab34d93c682a032ad52bb1ca4551be8d
Владислав Давиденко < >
2016-12-06 23:33:42 +0300

    blblbl

---
+++ MemberShip/MemberShip/Controllers/HomeController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MemberShip.Models;
+
+namespace MemberShip.Controllers
+{
+
+    public class HomeController : Controller
+    {
+
+        private Test db = new Test();
+        
+        //
+        // GET: /Home/
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View(db.UserProfile.ToList());
+        }
+
+        [Authorize]
+        public ActionResult Details(int? id)
+        {
+            UserProfile user = db.UserProfile.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        //
+        // GET: /Default/Create
+        [Authorize(Roles = "Admin")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Default/Create
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public ActionResult Create(UserProfile user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserProfile.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        //
+        // GET: /Default/Edit
+        [Authorize(Roles = "Admin")]
+        public ActionResult Edit(int? id)
+        {
+            UserProfile user = db.UserProfile.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        //
+        // POST: /Default/Edit
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public ActionResult Edit(UserProfile user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        //
+        // GET: /Default/Delete
+        [Authorize(Roles = "Admin")]
+        public ActionResult Delete(int? id)
+        {
+            UserProfile user = db.UserProfile.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        //
+        // POST: /Default/Delete
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public ActionResult Delete(UserProfile user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserProfile.Remove(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+    }
+}
---
+++ MemberShip/MemberShip/Models/AccountModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MemberShip.Models
+{
+    public class UsersContext : DbContext
+    {
+        public UsersContext()
+            : base("Test")
+        {
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+
+    //[Table("userprofile")]
+    //public class userprofile
+    //{
+    //    [Key]
+    //    [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+    //    public int userid { get; set; }
+    //    public string username { get; set; }
+    //}
+
+    public class LoginModel
+    {
+        [Required]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Display(Name = "Запомнить меня")]
+        public bool RememberMe { get; set; }
+    }
+}
